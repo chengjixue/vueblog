7968cdb77aca6d2edc37bbb01435c98e68f7a902
xuecj <xuecjpersonal@163.com>
2022-04-27 15:49:57 +0800

    初始化项目，增加reame.md

---
+++ src/test/java/edu/xcj/VueblogApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.xcj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
